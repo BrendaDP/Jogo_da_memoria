5bd29949c734a03a8e50ef4a93fae0b54d96646e
BrendaDP <brendapaula.pl014@academico.ifsul.com.br>
2026-06-19 09:06:53 -0300

    Testando codigos

---
+++ Assets/Scripts/Trocarcena.cs
@@ -0,0 +1,23 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trocarcena : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void trocarcena(string qualcena)
+    {
+        SceneManager.LoadScene(qualcena);
+    }
+}
